0c111109cb9a6260917533ef0be359abeb3b2ec0
powcoder <powcoder@163.com>
2022-11-22 12:47:38 +0800

    COMP9313 Project 1 C2LSH

---
+++ submission.py
@@ -0,0 +1,10 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+## import modules here
+
+########## Question 1 ##########
+# do not change the heading of the function
+def c2lsh(data_hashes, query_hashes, alpha_m, beta_n):
+    pass # **replace** this line with your code
